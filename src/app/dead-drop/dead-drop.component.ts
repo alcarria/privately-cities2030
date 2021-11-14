41b7b4133467e3e4e24195a33f3bb519d3c969f1
Diego GS <dieguete.gs@gmail.com>
2021-11-14 18:04:35 +0100

    Añadir envio de mensajes al contrato

--- src/app/dead-drop/dead-drop.component.ts
+++ src/app/dead-drop/dead-drop.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+// @ts-ignore
+import DeadDrop from '../../assets/contracts/DeadDrop.json'
 declare const window: any;
 
 @Component({
@@ -11,11 +13,16 @@ export class DeadDropComponent implements OnInit {
   private selectedAddress = 'PEDRO'
   private dummy = new Map([['PEDRO', ['HOLA PEDRO', 'que tal??']], ['SERGIO', ['HOLA SERGIO', 'que tal??']], ['PACO', ['HOLA PACO', 'que tal??']]])
 
+  private contract = new window.web3.eth.Contract(
+    DeadDrop.abi,
+    '0x489aDF7082939e60b407C8E75c472D7e3500832e'
+  )
+
   constructor() {
   }
 
-  async ngOnInit(): Promise<void> { 
-    console.log('Esta Web3 cargado?: '+ window.web3);
+  async ngOnInit(): Promise<void> {
+    
   }
 
   newChat(): void {
@@ -34,9 +41,13 @@ export class DeadDropComponent implements OnInit {
     return this.dummy.get(this.selectedAddress)
   }
 
-  sendMessage(message: any): void {
+  async sendMessage(message: any): Promise<void> {
     // todo
     // Enviarlo a la red
-    console.log(window.web3)
+    let addresses = await window.ethereum.request({ method: 'eth_accounts' });
+    this.contract.methods.sendMessage(this.selectedAddress, message).send({from: addresses[0]})
+      .then((receipt: any) => {
+        console.log(receipt)
+      })
   }
 }
