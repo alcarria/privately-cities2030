b7c8c241410be0736ce773d6a638a96508c86adc
Arturo <arturoholgado@gmail.com>
2021-11-15 19:13:37 +0100

    Añadido paso para añadir clave publica al contrato

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+// @ts-ignore
+import DeadDrop from "../../assets/contracts/DeadDrop.json";
+import {environment} from "../../environments/environment";
+
 declare let window: any
 
 @Component({
@@ -9,13 +13,21 @@ declare let window: any
 })
 export class LoginComponent implements OnInit {
 
+  private contract = new window.web3.eth.Contract(
+    DeadDrop.abi,
+    environment.deaddrop_address
+  )
+
+  metamask_accounts = ""
   loading = false
   error = ""
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+  }
 
   async ngOnInit(): Promise<void> {
-    const permissions = await window.ethereum.request({ method: 'wallet_getPermissions' });
+    const permissions = await window.ethereum.request({method: 'wallet_getPermissions'});
+    this.metamask_accounts = await window.ethereum.request({method: 'eth_accounts'});
 
     if (Array.isArray(permissions) && permissions.length) {
       console.log('DApp permissions:')
@@ -24,15 +36,42 @@ export class LoginComponent implements OnInit {
   }
 
   onLogin(): void {
+    //TODO: añadir demas contratos
     this.loading = true
+
+    //Solicitamos que inicie sesion con Metamask
     window.ethereum
       .request({
         method: 'eth_requestAccounts'
       })
       .then((result: any) => {
-        this.router.navigate(['dead-drop'])
+        let publicKey = ""
+        // Cuando ha iniciado sesion le pedimos permiso para añadir la publica al contrato
+        window.ethereum.request({
+          method: 'eth_getEncryptionPublicKey',
+          params: [this.metamask_accounts[0]]
+        })
+          .then((result: any) => {
+            publicKey = result;
+            //Comprobamos si la clave publica esta en el contrato
+            this.contract.methods.getPublicKey(this.metamask_accounts[0]).call().then((result: any) => {
+              if (result !== publicKey) {
+                //Si no esta, la añadimos
+                this.contract.methods.setPublicKey(this.metamask_accounts[0], publicKey).send({from: this.metamask_accounts[0]})
+                  .then((result: any) => {
+                    //Cuando se ha añadido su clave publica al contrato se sigue adelante en el proceso de login
+                    this.router.navigate(['dead-drop'])
+                  })
+              } else {
+                //Si esta, no la añadimos
+                this.router.navigate(['dead-drop'])
+              }
+            })
+
+          })
       })
       .catch((error: any) => {
+        console.log(error)
         this.error = "No se ha podido conectar con MetaMask."
       })
       .finally(() => {
--- truffle/contracts/DeadDrop.sol
+++ truffle/contracts/DeadDrop.sol
@@ -7,7 +7,9 @@ contract DeadDrop {
   // Send a message
   event SendMessage(string to, string message);
 
-  function shareSeed(address to, string memory seed) public{
+  mapping(address => string) public publicKeys;
+
+  function shareSeed(address to, string memory seed) public {
     // this function share with to the initial seed for startChat
     emit ShareSeed(to, seed);
   }
@@ -15,5 +17,13 @@ contract DeadDrop {
   function sendMessage(string memory to, string memory message) public {
     // this function send the message to to
     emit SendMessage(to, message);
+  }
+
+  function setPublicKey(address my_address, string memory publicKey) public {
+    publicKeys[my_address] = publicKey;
+  }
+
+  function getPublicKey(address dest_address) public view returns(string memory publicKey) {
+    publicKey = publicKeys[dest_address];
   }
 }
