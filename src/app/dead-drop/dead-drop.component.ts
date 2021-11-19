9d300c00ec9fb288823309f9dc7e3d768304f038
Diego GS <dieguete.gs@gmail.com>
2021-11-19 17:25:23 +0100

    Añadir recepcion mensajes

--- src/app/dead-drop/dead-drop.component.ts
+++ src/app/dead-drop/dead-drop.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import { environment } from '../../environments/environment';
+
+import { encrypt, decrypt } from '../modules/encryption.module'
+
 // @ts-ignore
 import DeadDrop from '../../assets/contracts/DeadDrop.json'
 declare const window: any;
@@ -12,21 +15,39 @@ declare const window: any;
 export class DeadDropComponent implements OnInit {
 
   private selectedAddress = 'PEDRO'
-  private dummy = new Map([['PEDRO', ['HOLA PEDRO', 'que tal??']], ['SERGIO', ['HOLA SERGIO', 'que tal??']], ['PACO', ['HOLA PACO', 'que tal??']]])
+  private dummy: Map<string, string[]> = new Map([['PEDRO', ['HOLA PEDRO', 'que tal??']], ['SERGIO', ['HOLA SERGIO', 'que tal??']], ['PACO', ['HOLA PACO', 'que tal??']]])
 
   private contract = new window.web3.eth.Contract(
     DeadDrop.abi,
     environment.deaddrop_address
   )
 
-  constructor() {
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log('onInit: ' + this.dummy)
+    console.log('Holaaaa')
+    this.contract.events.SendMessage({
+      fromBlock: 0
+    }, (error: any, event: any) => this.onMessageEvent(error, event))
   }
 
-  async ngOnInit(): Promise<void> {
+  onMessageEvent(error: any, event: any): void {
+    let to = String(event.returnValues.to)
     
+    // @ts-ignore
+    let messages: string[] = this.dummy.get(to) == undefined ? [] : this.dummy.get(to)
+
+    if (messages == undefined) messages = []
+
+    messages.push(event.returnValues.message)
+
+    this.dummy.set(to, messages)
+    console.log(event.returnValues.message)
   }
 
   newChat(): void {
+    console.log(this.dummy)
     this.dummy.set('Chat ' + this.dummy.size, [])
   }
 
@@ -44,9 +65,10 @@ export class DeadDropComponent implements OnInit {
 
   async sendMessage(message: any): Promise<void> {
     // todo
+    let encryptedMessage = encrypt(message, '', {})
     // Enviarlo a la red
     let addresses = await window.ethereum.request({ method: 'eth_accounts' });
-    this.contract.methods.sendMessage(this.selectedAddress, message).send({from: addresses[0]})
+    this.contract.methods.sendMessage(this.selectedAddress, encryptedMessage).send({from: addresses[0]})
       .then((receipt: any) => {
         console.log(receipt)
       })
---
+++ src/app/modules/encryption.module.ts
@@ -0,0 +1,7 @@
+export function encrypt(data: String, algorithm: String, params: {}): String {
+    return data
+}
+
+export function decrypt(data: String, algorithm: String, params: {}): String {
+    return data
+}
