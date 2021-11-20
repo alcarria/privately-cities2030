0a8fa1c328a89cc6291545c9742e9bb200f31355
Diego GS <dieguete.gs@gmail.com>
2021-11-20 11:47:21 +0100

    Añadir current address en navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare const window: any
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  public account: string = ''
+
   constructor() { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    let accounts = await window.ethereum.request({ method: 'eth_accounts' })
+    this.account = accounts[0]
   }
-
 }
